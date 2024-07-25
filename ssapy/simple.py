65ceecd6eb18e67e0141b45725a392ad2df57f3c
Travis Yeager <43413776+SuperdoerTrav@users.noreply.github.com>
2024-07-25 16:35:25 -0700

    New plots (#11)
    * new file to handle coordinate conversions
    
    * moved most of the coordinate functions to .coordinates, some remain as they will require much more refactoring
    
    * added several functions for use in plotUtils
    
    * improved the brightness functions and added a gamma angle calculation
    
    * updated the calculate_orbital_elements function
    
    * added keplerian orbital element plots
    
    * simply.py is a useful file to add functions that combine many other functionalities within ssapy.
    
    * added points_on_circle for use in simply.py
    
    * added ipython module for interactive plots
    
    * added imageio for generation of gifs.
    
    * added tqdm for progress bars
    
    * added faster io functions and updated old io functions added by me
    
    * added functions to calculate the position of lagrange points
    
    * removed due to circular import conflicts, coordinates are all together in utils.py where they initially were
    
    * added many more coordinate conversions useful for plotting
    
    * added a gif writing function
    
    * added built in plot to rotate_vector - used for validation
    
    * fixed remaining circular imports
    
    * cleaned up the get_times function, added weeks as a unit
    
    * added doc string to ssapy_orbit()
    
    * added plot_path as optional input to rotate_vector()
    
    * added simply.py which will contain simplified functions for using ssapy
    
    * adding a testing plot testing function
    
    * changed the output of ssapy_orbit() to always give back the time array used.
    
    * changed save directory of test plots
    
    * added an ignore for the ssapy_test_plots/ folder which will be generated if user runs test_plots.py
    
    * added docstrings to all plot functions
    
    * added doc strings to all new functions
    
    * added doc strings
    
    * changed lagrange_point_lunar_frame() doc string
    
    * more doc strings
    
    * added doc strings
    
    * added / after {save_folder}
    
    * empty line at end
    
    * doc string to DRO()
    
    * new line added
    
    * mentioned Kelerian in the doc string
    
    * file_exists renamed
    
    * renamed getAngle and moved get_angle to utils.py where a copy was already.
    
    * renamed file_exists
    
    * modified save_plot to accept any extension
    
    * renamed write gif to save_animated_gif
    
    * removed a comment
    
    * removed get_angle()
    
    * changed doc strings to lagrange point functions
    
    * changed some function names that were changed in .utils and .compute
    
    * merged make_black and make_white to set_color_theme
    
    * renamed groundTrackPlot and added Keplerian to several dot strings
    
    * drawmoon and drawearth renamed
    
    * standardized npload and npsave and pload psave
    
    * more renamed and changes
    
    * lunar decapitalized
    
    * bool instantly returned in exists
    
    * index added to sortbynum to allow user to chose which number to sort by
    
    * sortbynum renamed to _sortbynum
    
    ---------
    
    Co-authored-by: yeager7 <yeager7@llnl.gov>

---
+++ ssapy/simple.py
@@ -0,0 +1,277 @@
+# flake8: noqa: E501
+
+"""
+Functions to simplify certain aspects of SSAPy. 
+For when you want a quick answer and do not need want to use high fidelity defaults.
+"""
+
+from .utils import get_times, points_on_circle
+from . import Orbit, rv
+from .accel import AccelKepler, AccelSolRad, AccelEarthRad, AccelDrag
+from .body import get_body
+from .gravity import AccelHarmonic, AccelThirdBody
+from .propagator import RK78Propagator
+
+from astropy.time import Time
+import numpy as np
+
+
+def keplerian_prop(integration_timestep: float = 10) -> RK78Propagator:
+    """
+    Create and return an RK78Propagator for a Keplerian orbit.
+
+    This propagator uses only the Keplerian acceleration for a two-body problem.
+
+    Parameters:
+    ----------
+    integration_timestep : float, optional
+        The time step for the RK78Propagator integration (default is 10 seconds).
+
+    Returns:
+    -------
+    RK78Propagator
+        An instance of RK78Propagator configured with Keplerian acceleration.
+    """
+    return RK78Propagator(AccelKepler(), h=integration_timestep)
+
+
+accel_3_cache = None
+def threebody_prop(integration_timestep: float = 10) -> RK78Propagator:
+    """
+    Create and return an RK78Propagator with a set of accelerations for a three-body problem.
+
+    The three bodies considered are Earth (Keplerian effect), Moon, and the Earth itself.
+
+    Parameters:
+    ----------
+    integration_timestep : float, optional
+        The time step for the RK78Propagator integration (default is 10 seconds).
+
+    Returns:
+    -------
+    RK78Propagator
+        An instance of RK78Propagator configured with the three-body accelerations.
+    """
+    global accel_3_cache
+    if accel_3_cache is None:
+        accel_3_cache = AccelKepler() + AccelThirdBody(get_body("moon"))
+    return RK78Propagator(accel_3_cache, h=integration_timestep)
+
+
+accel_4_cache = None
+def fourbody_prop(integration_timestep: float = 10) -> RK78Propagator:
+    """
+    Create and return an RK78Propagator with a set of accelerations for a four-body problem.
+
+    The four bodies considered are Earth (Keplerian effect), Moon, Sun, and the Earth itself.
+
+    Parameters:
+    ----------
+    integration_timestep : float, optional
+        The time step for the RK78Propagator integration (default is 10 seconds).
+
+    Returns:
+    -------
+    RK78Propagator
+        An instance of RK78Propagator configured with the four-body accelerations.
+    """
+    global accel_4_cache
+    if accel_4_cache is None:
+        accel_4_cache = AccelKepler() + AccelThirdBody(get_body("moon")) + AccelThirdBody(get_body("Sun"))
+    return RK78Propagator(accel_4_cache, h=integration_timestep)
+
+
+accel_best_cache = None
+def best_prop(integration_timestep=10, kwargs=dict(mass=250, area=.022, CD=2.3, CR=1.3)):
+    """
+    Create and return an RK78Propagator with a comprehensive set of accelerations.
+
+    Parameters:
+    ----------
+    integration_timestep : float, optional
+        The time step for the RK78Propagator integration (default is 10 seconds).
+    kwargs : dict, optional
+        Dictionary of parameters for non-conservative forces (mass, area, CD, CR).
+        If not provided, defaults are used.
+
+    Returns:
+    -------
+    RK78Propagator
+        An instance of RK78Propagator configured with cached accelerations.
+    """
+    global accel_best_cache
+    if accel_best_cache is None:
+        aEarth = AccelKepler() + AccelHarmonic(get_body("Earth", model="EGM2008"), 140, 140)
+        aMoon = AccelThirdBody(get_body("moon")) + AccelHarmonic(get_body("moon"), 20, 20)
+        aSun = AccelThirdBody(get_body("Sun"))
+        aMercury = AccelThirdBody(get_body("Mercury"))
+        aVenus = AccelThirdBody(get_body("Venus"))
+        aMars = AccelThirdBody(get_body("Mars"))
+        aJupiter = AccelThirdBody(get_body("Jupiter"))
+        aSaturn = AccelThirdBody(get_body("Saturn"))
+        aUranus = AccelThirdBody(get_body("Uranus"))
+        aNeptune = AccelThirdBody(get_body("Neptune"))
+        nonConservative = AccelSolRad(**kwargs) + AccelEarthRad(**kwargs) + AccelDrag(**kwargs)
+        planets = aMercury + aVenus + aMars + aJupiter + aSaturn + aUranus + aNeptune
+        accel_best_cache = aEarth + aMoon + aSun + planets + nonConservative
+    return RK78Propagator(accel_best_cache, h=integration_timestep)
+
+
+def ssapy_kwargs(mass=250, area=0.022, CD=2.3, CR=1.3):
+    """
+    Generate a dictionary of default parameters for a space object used in simulations.
+
+    Parameters:
+    ----------
+    mass : float, optional
+        Mass of the object in kilograms (default is 250 kg).
+    area : float, optional
+        Cross-sectional area of the object in square meters (default is 0.022 m^2).
+    CD : float, optional
+        Drag coefficient of the object (default is 2.3).
+    CR : float, optional
+        Radiation pressure coefficient of the object (default is 1.3).
+
+    Returns:
+    -------
+    dict
+        A dictionary containing the parameters for the space object.
+    """
+    # Asteroid parameters
+    kwargs = dict(
+        mass=mass,  # [kg]
+        area=area,  # [m^2]
+        CD=CD,  # Drag coefficient
+        CR=CR,  # Radiation pressure coefficient
+    )
+    return kwargs
+
+
+def ssapy_prop(integration_timestep=60, propkw=ssapy_kwargs()):
+    """
+    Setup and return an RK78 propagator with specified accelerations and radiation pressure effects.
+
+    Parameters:
+    ----------
+    integration_timestep : int
+        Time step for the numerical integration (in seconds).
+    propkw : dict, optional
+        Keyword arguments for radiation pressure accelerations. If None, default arguments are used.
+
+    Returns:
+    -------
+    RK78Propagator
+        An RK78 propagator configured with the specified accelerations and time step.
+    """
+    # Accelerations - pass a body object or string of body name.
+    moon = get_body("moon")
+    sun = get_body("Sun")
+    Mercury = get_body("Mercury")
+    Venus = get_body("Venus")
+    Earth = get_body("Earth", model="EGM2008")
+    Mars = get_body("Mars")
+    Jupiter = get_body("Jupiter")
+    Saturn = get_body("Saturn")
+    Uranus = get_body("Uranus")
+    Neptune = get_body("Neptune")
+    aEarth = AccelKepler() + AccelHarmonic(Earth, 140, 140)
+    aSun = AccelThirdBody(sun)
+    aMoon = AccelThirdBody(moon) + AccelHarmonic(moon, 20, 20)
+    aSolRad = AccelSolRad(**propkw)
+    aEarthRad = AccelEarthRad(**propkw)
+    accel = aEarth + aMoon + aSun + aSolRad + aEarthRad
+    # Build propagator
+    prop = RK78Propagator(accel, h=integration_timestep)
+    return prop
+
+
+# Uses the current best propagator and acceleration models in ssapy
+def ssapy_orbit(orbit=None, a=None, e=0, i=0, pa=0, raan=0, ta=0, r=None, v=None, duration=(30, 'day'), freq=(1, 'hr'), t0=Time("2025-01-01", scale='utc'), t=None, prop=ssapy_prop()):
+    """
+    Compute the orbit of a spacecraft given either Keplerian elements or position and velocity vectors.
+
+    Parameters:
+    - orbit (Orbit, optional): An Orbit object if you already have an orbit defined.
+    - a (float, optional): Semi-major axis of the orbit in meters.
+    - e (float, optional): Eccentricity of the orbit (default is 0, i.e., circular orbit).
+    - i (float, optional): Inclination of the orbit in degrees.
+    - pa (float, optional): Argument of perigee in degrees.
+    - raan (float, optional): Right ascension of the ascending node in degrees.
+    - ta (float, optional): True anomaly in degrees.
+    - r (array-like, optional): Position vector in meters.
+    - v (array-like, optional): Velocity vector in meters per second.
+    - duration (tuple, optional): Duration of the simulation as a tuple (value, unit), where unit is 'day', 'hour', etc. Default is 30 days.
+    - freq (tuple, optional): Frequency of the output as a tuple (value, unit), where unit is 'day', 'hour', etc. Default is 1 hour.
+    - t0 (str, optional): Start date of the simulation in 'YYYY-MM-DD' format. Default is "2025-01-01".
+    - t (array-like, optional): Specific times at which to compute the orbit. If None, times will be generated based on duration and frequency.
+    - prop (function, optional): A function to compute the perturbation effects. Default is `ssapy_prop()`.
+
+    Returns:
+    - r (array-like): Position vectors of the spacecraft at the specified times.
+    - v (array-like): Velocity vectors of the spacecraft at the specified times.
+    - t (array-like): Times at which the orbit was computed. Returned only if `t` was None.
+
+    Raises:
+    - ValueError: If neither Keplerian elements nor position and velocity vectors are provided.
+    - RuntimeError or ValueError: If an error occurs during computation.
+    """
+    if t is None:
+        t = get_times(duration=duration, freq=freq, t=t0)
+
+    if orbit is not None:
+        pass
+    elif a is not None:
+        kElements = [a, e, i, pa, raan, ta]
+        orbit = Orbit.fromKeplerianElements(*kElements, t0)
+    elif r is not None and v is not None:
+        orbit = Orbit(r, v, t0)
+    else:
+        raise ValueError("Either Keplerian elements (a, e, i, pa, raan, ta) or position and velocity vectors (r, v) must be provided.")
+
+    try:
+        r, v = rv(orbit, t, prop)
+        return r, v, t
+    except (RuntimeError, ValueError) as err:
+        print(err)
+        return np.nan, np.nan, np.nan
+
+
+def get_similar_orbits(r0, v0, rad=1e5, num_orbits=4, duration=(90, 'days'), freq=(1, 'hour'), start_date="2025-1-1", mass=250):
+    """
+    Generate similar orbits by varying the initial position.
+
+    Parameters:
+    ----------
+    r0 : array_like
+        Initial position vector of shape (3,).
+    v0 : array_like
+        Initial velocity vector of shape (3,).
+    rad : float
+        Radius of the circle around the initial position to generate similar orbits.
+    num_orbits : int
+        Number of similar orbits to generate.
+    duration : tuple
+        Duration of the orbit simulation.
+    freq : tuple
+        Frequency of output data.
+    start_date : str
+        Start date for the simulation.
+    mass : float
+        Mass of the satellite.
+
+    Returns:
+    -------
+    trajectories : ndarray
+        Stacked array of shape (3, n_times, num_orbits) containing the trajectories.
+    """
+    r0 = np.reshape(r0, (1, 3))
+    v0 = np.reshape(v0, (1, 3))
+    print(r0, v0)
+    for idx, point in enumerate(points_on_circle(r0, v0, rad=rad, num_points=num_orbits)):
+        # Calculate entire satellite trajectory
+        r, v = ssapy_orbit(r=point, v=v0, duration=duration, freq=freq, start_date=start_date, integration_timestep=10, mass=mass, area=mass / 19000 + 0.01, CD=2.3, CR=1.3)
+        if idx == 0:
+            trajectories = np.concatenate((r0, v0), axis=1)[:len(r)]
+        rv = np.concatenate((r, v), axis=1)
+        trajectories = np.dstack((trajectories, rv))
+    return trajectories
---
+++ tests/test_plots.py
@@ -0,0 +1,170 @@
+import ssapy
+import os
+import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+
+save_folder = os.path.expanduser('./ssapy_test_plots/')
+print(f"Putting test_plot.py output in: {save_folder}")
+
+# Testing rotate_vector() in utils.
+v_unit = np.array([1, 0, 0])  # Replace this with your actual unit vector
+
+figs = []
+
+temp_directory = f'{save_folder}/rotate_vector_frames/'
+os.makedirs(temp_directory, exist_ok=True)
+
+i = 0
+for theta in range(0, 181, 20):
+    for phi in range(0, 361, 20):
+        new_unit_vector = ssapy.utils.rotate_vector(v_unit, theta, phi, plot_path=temp_directory, save_idx=i)
+        i += 1
+
+gif_path = f"{save_folder}/rotate_vectors_{v_unit[0]:.0f}_{v_unit[1]:.0f}_{v_unit[2]:.0f}.gif"
+ssapy.plotUtils.save_animated_gif(gif_name=gif_path, frames=ssapy.io.listdir(f'{temp_directory}*', sorted=True), fps=20)
+shutil.rmtree(temp_directory)
+
+
+# Creating orbit plots
+times = ssapy.utils.get_times(duration=(1, 'year'), freq=(1, 'hour'), t='2025-3-1')
+moon = ssapy.get_body("moon").position(times).T
+
+
+def initialize_DRO(t, delta_r=7.52064e7, delta_v=344):
+    """
+    Calculate an orbit with adjustments based on the Moon's position and velocity.
+
+    Parameters:
+    ----------
+    t : Time
+        The time at which to calculate the orbit.
+    delta_r : float, optional
+        The adjustment to the Moon's position (default is 7.52064e7 meters).
+    delta_v : float, optional
+        The adjustment to the Moon's velocity (default is 344 meters/second).
+
+    Returns:
+    -------
+    Orbit
+        SSAPy Orbit object.
+    """
+    moon = ssapy.get_body("moon")
+
+    unit_vector_moon = moon.position(t) / np.linalg.norm(moon.position(t))
+    moon_v = (moon.position(t.gps) - moon.position(t.gps - 1)) / 1
+    unit_vector_moon_velocity = moon_v / np.linalg.norm(moon_v)
+    ssapy.compute.lunar_lagrange_points(t=times[0])
+
+    r = (np.linalg.norm(moon.position(t)) - delta_r) * unit_vector_moon
+    v = (np.linalg.norm(moon_v) + delta_v) * unit_vector_moon_velocity
+
+    orbit = ssapy.Orbit(r=r, v=v, t=t)
+    return orbit
+
+
+# DRO
+dro_orbit = initialize_DRO(t=times[0])
+r, v, t = ssapy.simple.ssapy_orbit(orbit=dro_orbit, t=times)
+ssapy.plotUtils.orbit_plot(r=r, t=times, save_path=f"{save_folder}/DRO_orbit", frame='Lunar', show=False)
+r_lunar, v_lunar = ssapy.utils.gcrf_to_lunar_fixed(r, t=times, v=True)
+print("Succesfully converted gcrf to lunar frame.")
+ssapy.plotUtils.koe_plot(r, v, t=times, body='Earth', save_path=f"{save_folder}Keplerian_orbital_elements.png")
+
+ssapy.plotUtils.orbit_plot(r=r, t=times, save_path=f"{save_folder}/gcrf_plot.png", frame='gcrf', show=True)
+ssapy.plotUtils.orbit_plot(r=r, t=times, save_path=f"{save_folder}/itrf_plot", frame='itrf', show=True)
+ssapy.plotUtils.orbit_plot(r=r, t=times, save_path=f"{save_folder}/lunar_plot", frame='lunar', show=True)
+ssapy.plotUtils.orbit_plot(r=r, t=times, save_path=f"{save_folder}/lunar_axis_lot", frame='lunar axis', show=True)
+print(f"Created a GCRF orbit plot.")
+print(f"Created a ITRF orbit plot.")
+print(f"Created a Lunar orbit plot.")
+print(f"Created a Lunar axis orbit plot.")
+
+# Globe plot of GTO Orbit
+r_geo, _, t_geo = ssapy.simple.ssapy_orbit(a=ssapy.constants.RGEO, e=0.3, t=times)
+ssapy.plotUtils.globe_plot(r=r_geo, t=t_geo, save_path=f"{save_folder}/globe_plot", scale=5)
+print('Created a globe plot.')
+
+ssapy.plotUtils.ground_track_plot(r=r_geo, t=t_geo, ground_stations=None, save_path=f"{save_folder}/ground_track_plot")
+print('Created a ground track plot.')
+
+# Example usage
+earth_pos = np.array([0, 0, 0])  # Earth at the origin
+moon_pos = ssapy.get_body("moon").position(times[0]).T
+
+# Plotting
+fig = plt.figure(figsize=(8, 8))
+fig.patch.set_facecolor('white')
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot Earth
+ax.scatter(earth_pos[0], earth_pos[1], earth_pos[2], color='blue', label='Earth')
+ax.text(earth_pos[0], earth_pos[1], earth_pos[2], 'Earth', color='blue')
+
+# Plot Moon
+ax.scatter(moon_pos[0], moon_pos[1], moon_pos[2], color='grey', label='Moon')
+ax.text(moon_pos[0], moon_pos[1], moon_pos[2], 'Moon', color='grey')
+
+# Plot Lagrange points
+colors = ['red', 'green', 'purple', 'orange', 'cyan']
+for (point, pos), color in zip(ssapy.compute.lunar_lagrange_points(t=times[0]).items(), colors):
+    ax.scatter(pos[0], pos[1], pos[2], color=color, label=point)
+    ax.text(pos[0], pos[1], pos[2], point, color=color)
+
+# Add a dashed black circle at distance LD
+current_LD = np.linalg.norm(moon_pos, axis=-1)
+normal_vector = ssapy.compute.moon_normal_vector(t=times[0])
+ssapy.plotUtils.draw_dashed_circle(ax, normal_vector, current_LD, 12)
+ax.quiver(0, 0, 0, normal_vector[0], normal_vector[1], normal_vector[2], color='r', length=1)
+
+# Labels and legend
+ax.set_xlabel('X (m)')
+ax.set_ylabel('Y (m)')
+ax.set_zlabel('Z (m)')
+ax.set_title("Lunar Lagrange Points using Moon's true position")
+ax.axis('equal')
+ax.legend()
+plt.show()
+ssapy.plotUtils.save_plot(fig, save_path=f"{save_folder}/lagrange_points")
+
+# Plotting
+fig = plt.figure(figsize=(8, 8))
+fig.patch.set_facecolor('white')
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot Earth
+ax.scatter(earth_pos[0], earth_pos[1], earth_pos[2], color='blue', label='Earth')
+ax.text(earth_pos[0], earth_pos[1], earth_pos[2], 'Earth', color='blue')
+
+# Plot Moon
+ax.scatter(moon_pos[0], moon_pos[1], moon_pos[2], color='grey', label='Moon')
+ax.text(moon_pos[0], moon_pos[1], moon_pos[2], 'Moon', color='grey')
+
+# Plot Lagrange points
+colors = ['red', 'green', 'purple', 'orange', 'cyan']
+for (point, pos), color in zip(ssapy.compute.lunar_lagrange_points_circular(t=times[0]).items(), colors):
+    ax.scatter(pos[0], pos[1], pos[2], color=color, label=point)
+    ax.text(pos[0], pos[1], pos[2], point, color=color)
+
+# Add a dashed black circle at distance LD
+current_LD = np.linalg.norm(moon_pos, axis=-1)
+normal_vector = ssapy.compute.moon_normal_vector(t=times[0])
+ssapy.plotUtils.draw_dashed_circle(ax, normal_vector, current_LD, 12)
+ax.quiver(0, 0, 0, normal_vector[0], normal_vector[1], normal_vector[2], color='r', length=1)
+
+# Labels and legend
+ax.set_xlabel('X (m)')
+ax.set_ylabel('Y (m)')
+ax.set_zlabel('Z (m)')
+ax.set_title('Lunar Lagrange Points assuming circular orbit.')
+ax.axis('equal')
+ax.legend()
+plt.show()
+ssapy.plotUtils.save_plot(fig, save_path=f"{save_folder}/lagrange_points")
+
+print(f"Lagrange points were calculated correctly.")
+print(f"Rotate vector plot successfully created.")
+print(f"save_plot() executed succesfully.")
+print(f"save_animated_gif() executed succesfully.")
+
+print(f"\nFinished plot testing!\n")
