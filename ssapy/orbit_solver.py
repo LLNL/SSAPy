ba6ed3ddf35702296d883c395093e765bffbba0d
Jason Bernstein <bernstein8@llnl.gov>
2024-09-30 22:15:23 -0600

    Add full citations for later references

--- ssapy/orbit_solver.py
+++ ssapy/orbit_solver.py
@@ -12,6 +12,10 @@ DanchickTwoPosOrbitSolver:
 SheferTwoPosOrbitSolver:
 
 ThreeAngleOrbitSolver
+
+References:
+- Shefer, V. A. (2010). New method of orbit determination from two position vectors based on solving Gauss’s equations. Solar System Research, 44, 252-266.
+- Montenbruck, O., Gill, E., & Lutze, F. H. (2002). Satellite orbits: models, methods, and applications. Appl. Mech. Rev., 55(2), B27-B28.
 """
 
 
