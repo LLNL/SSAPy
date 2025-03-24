cb1a09bf8d2cb76ba42d2bd929ba1bf6c1af21c1
dehiggin <higgins30@llnl.gov>
2025-03-24 15:58:42 -0700

    mocked objects

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,6 @@ import subprocess
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../')) 
-autodoc_mock_imports = ['ssapy']
 
 
 # -- Project information -----------------------------------------------------
