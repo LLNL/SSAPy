c781336b6eb07c3af62a559bbed065c4c648684f
dehiggin <higgins30@llnl.gov>
2025-03-24 15:20:34 -0700

    tested install

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import subprocess
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../ssapy/'))
+# sys.path.insert(0, os.path.abspath('../../ssapy/'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
