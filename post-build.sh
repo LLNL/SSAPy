f8c38259fe5428c2e2fd4d6b061ed0be1a68768f
dehiggin <higgins30@llnl.gov>
2025-03-25 13:51:27 -0700

    test

--- post-build.sh
+++ post-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs -y
 git submodule update --init --recursive
 git lfs install
 git lfs pull
