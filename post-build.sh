e1b6573abeac44603f59272c017d28e18720fc26
dehiggin <higgins30@llnl.gov>
2025-03-25 13:55:16 -0700

    test

--- post-build.sh
+++ post-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pip install git-lfs
+apt update
+apt install build-essential git git-lfs python3 python3-distutils python3-venv graphviz
 git submodule update --init --recursive
 git lfs install
 git lfs pull
