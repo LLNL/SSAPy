8be6a37748d1791b6a1bbcea824f64d4a9d0749a
dehiggin <higgins30@llnl.gov>
2025-04-29 15:13:21 -0700

    path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,8 @@
 import os
 import sys
 import ssapy
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
+
 # sys.path.insert(0, os.path.abspath('../../ssapy'))
 # print(sys.path)
 
