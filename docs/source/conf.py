217a2bb11a6bb648e43859463da707a13cb3e018
dehiggin <higgins30@llnl.gov>
2025-04-29 13:47:34 -0700

    path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../ssapy'))
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
