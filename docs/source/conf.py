7309326a1c6a564a15ef70bea738afab25846984
dehiggin <higgins30@llnl.gov>
2025-04-29 13:05:39 -0700

    testing imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../ssapy'))
+autodoc_mock_imports = ['ssapy']
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
