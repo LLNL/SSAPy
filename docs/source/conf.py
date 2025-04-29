06c380e6811e5660d1ce943420866ade60cadb93
dehiggin <higgins30@llnl.gov>
2025-04-29 13:16:25 -0700

    verbose

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../ssapy'))
-autodoc_mock_imports = ['ssapy']
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
