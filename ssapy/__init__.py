d0f41ce551855438bbfebc7320215f23566fcadb
Denvir <denvir.j.higgins@gmail.com>
2025-04-29 20:37:26 -0700

    debugging

--- ssapy/__init__.py
+++ ssapy/__init__.py
@@ -4,7 +4,7 @@ ensure_data_downloaded()
 import os
 datadir = os.path.join(os.path.dirname(__file__), "data")
 
-from ._ssapy import *
+from . import _ssapy
 
 from .orbit import Orbit, EarthObserver, OrbitalObserver
 from .propagator import (
