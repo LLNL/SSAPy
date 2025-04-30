1eff10f319407bb0223da0222eaa405020006b11
Denvir <denvir.j.higgins@gmail.com>
2025-04-29 21:02:35 -0700

    testing import

--- ssapy/__init__.py
+++ ssapy/__init__.py
@@ -4,43 +4,43 @@ ensure_data_downloaded()
 import os
 datadir = os.path.join(os.path.dirname(__file__), "data")
 
-from .orbit import Orbit, EarthObserver, OrbitalObserver
-from .propagator import (
-    KeplerianPropagator, SeriesPropagator, RK4Propagator, SGP4Propagator,
-    SciPyPropagator, RK78Propagator, RK8Propagator
-)
-from .compute import rv, dircos, radec, altaz, quickAltAz, radecRate, groundTrack
+# from .orbit import Orbit, EarthObserver, OrbitalObserver
+# from .propagator import (
+#     KeplerianPropagator, SeriesPropagator, RK4Propagator, SGP4Propagator,
+#     SciPyPropagator, RK78Propagator, RK8Propagator
+# )
+# from .compute import rv, dircos, radec, altaz, quickAltAz, radecRate, groundTrack
 
-from .accel import Accel, AccelKepler, AccelSum, AccelEarthRad, AccelSolRad, AccelDrag, AccelConstNTW
+# from .accel import Accel, AccelKepler, AccelSum, AccelEarthRad, AccelSolRad, AccelDrag, AccelConstNTW
 
-from .linker import ModelSelectorParams, BinarySelectorParams, Linker
+# from .linker import ModelSelectorParams, BinarySelectorParams, Linker
 
-from .orbit_solver import TwoPosOrbitSolver, GaussTwoPosOrbitSolver
-from .orbit_solver import DanchickTwoPosOrbitSolver, SheferTwoPosOrbitSolver
-from .orbit_solver import ThreeAngleOrbitSolver
+# from .orbit_solver import TwoPosOrbitSolver, GaussTwoPosOrbitSolver
+# from .orbit_solver import DanchickTwoPosOrbitSolver, SheferTwoPosOrbitSolver
+# from .orbit_solver import ThreeAngleOrbitSolver
 
-from .particles import Particles
+# from .particles import Particles
 
-from .rvsampler import GEOProjectionInitializer, DistanceProjectionInitializer
-from .rvsampler import circular_guess
-from .rvsampler import GaussianRVInitializer, DirectInitializer
-from .rvsampler import RVProbability, EmceeSampler, MVNormalProposal
-from .rvsampler import RVSigmaProposal, MHSampler, LMOptimizer
+# from .rvsampler import GEOProjectionInitializer, DistanceProjectionInitializer
+# from .rvsampler import circular_guess
+# from .rvsampler import GaussianRVInitializer, DirectInitializer
+# from .rvsampler import RVProbability, EmceeSampler, MVNormalProposal
+# from .rvsampler import RVSigmaProposal, MHSampler, LMOptimizer
 
-from .ellipsoid import Ellipsoid
+# from .ellipsoid import Ellipsoid
 
-from .body import (
-    EarthOrientation, MoonOrientation, MoonPosition, Body, get_body
-)
-from .gravity import HarmonicCoefficients, AccelThirdBody, AccelHarmonic
+# from .body import (
+#     EarthOrientation, MoonOrientation, MoonPosition, Body, get_body
+# )
+# from .gravity import HarmonicCoefficients, AccelThirdBody, AccelHarmonic
 
-# Some items we keep out of the ssa namespace, but still provide through
-# submodules
-from . import constants
-from . import plotUtils
-from . import io
-from . import utils
-from . import simple
+# # Some items we keep out of the ssa namespace, but still provide through
+# # submodules
+# from . import constants
+# from . import plotUtils
+# from . import io
+# from . import utils
+# from . import simple
 from astropy.time import Time, TimeDelta
 import astropy.units as u
 from datetime import timedelta
