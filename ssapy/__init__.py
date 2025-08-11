4a8640421f22bb7d51f265198479c1f6be62ed73
Higgins <higgins30@llnl.gov>
2025-08-11 09:17:36 -0700

    making the download smarter

--- ssapy/__init__.py
+++ ssapy/__init__.py
@@ -1,50 +1,59 @@
-from .data_utils import ensure_data_downloaded
-ensure_data_downloaded()
-
 import os
-datadir = os.path.join(os.path.dirname(__file__), "data")
 
+# Lazy data directory access
+def _get_datadir():
+    """Get data directory, downloading data if needed (lazy loading)."""
+    from .data_utils import get_data_dir
+    return get_data_dir()
+
+# Make datadir a property that downloads data when first accessed
+class _DataDir:
+    def __init__(self):
+        self._path = None
+    
+    def __str__(self):
+        if self._path is None:
+            self._path = _get_datadir()
+        return self._path
+    
+    def __fspath__(self):
+        return str(self)
+
+datadir = _DataDir()
+
+# All your existing imports
 from . import _ssapy
-
 from .orbit import Orbit, EarthObserver, OrbitalObserver
 from .propagator import (
     KeplerianPropagator, SeriesPropagator, RK4Propagator, SGP4Propagator,
     SciPyPropagator, RK78Propagator, RK8Propagator
 )
 from .compute import rv, dircos, radec, altaz, quickAltAz, radecRate, groundTrack
-
 from .accel import Accel, AccelKepler, AccelSum, AccelEarthRad, AccelSolRad, AccelDrag, AccelConstNTW
-
 from .linker import ModelSelectorParams, BinarySelectorParams, Linker
-
 from .orbit_solver import TwoPosOrbitSolver, GaussTwoPosOrbitSolver
 from .orbit_solver import DanchickTwoPosOrbitSolver, SheferTwoPosOrbitSolver
 from .orbit_solver import ThreeAngleOrbitSolver
-
 from .particles import Particles
-
 from .rvsampler import GEOProjectionInitializer, DistanceProjectionInitializer
 from .rvsampler import circular_guess
 from .rvsampler import GaussianRVInitializer, DirectInitializer
 from .rvsampler import RVProbability, EmceeSampler, MVNormalProposal
 from .rvsampler import RVSigmaProposal, MHSampler, LMOptimizer
-
 from .ellipsoid import Ellipsoid
-
 from .body import (
     EarthOrientation, MoonOrientation, MoonPosition, Body, get_body
 )
 from .gravity import HarmonicCoefficients, AccelThirdBody, AccelHarmonic
 
-# Some items we keep out of the ssa namespace, but still provide through
-# submodules
+# Keep submodules available
 from . import constants
 from . import plotUtils
 from . import io
 from . import utils
 from . import simple
+
+# External dependencies
 from astropy.time import Time, TimeDelta
 import astropy.units as u
 from datetime import timedelta
-
-
